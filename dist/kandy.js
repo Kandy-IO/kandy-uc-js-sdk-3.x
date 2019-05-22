c4f73710d5220b1a28fff1e13e13162cd46cfc84
machine <janwa039@gmail.com>
2019-05-22 21:11:45 +0000

    77973

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.uc.js
- * Version: 3.5.0-beta.77849
+ * Version: 3.5.0-beta.77973
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -16795,7 +16795,7 @@ const factoryDefaults = {
    */
 };function factory(plugins, options = factoryDefaults) {
   // Log the SDK's version (templated by webpack) on initialization.
-  let version = '3.5.0-beta.77849';
+  let version = '3.5.0-beta.77973';
   log.info(`UC SDK version: ${version}`);
 
   var sagas = [];
