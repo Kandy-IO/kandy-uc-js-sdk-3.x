10efca1b2cd55305b8d3dfbc4858789642038955
machine <janwa039@gmail.com>
2020-06-12 14:01:11 +0000

    441

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.uc.js
- * Version: 3.17.0-beta.440
+ * Version: 3.17.0-beta.441
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -61174,7 +61174,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '3.17.0-beta.440';
+  return '3.17.0-beta.441';
 }
 
 /***/ }),
